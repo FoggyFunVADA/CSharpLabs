97f0d2a6ce6745bed1a2bc7efa5bbef77428e1ef
Дмитрий Дедов <106934409+FoggyFunVADA@users.noreply.github.com>
2022-06-06 14:28:52 +0300

    Add files via upload

---
+++ EmployeesView/Lab3.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace EmployeesView
+{
+    internal static class Lab3
+    {
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new DirectorsForm());
+        }
+    }
+}
